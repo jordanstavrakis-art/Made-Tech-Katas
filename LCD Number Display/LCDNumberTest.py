dd06fca52a8adecbd975c6d92724b1de04de4e10
jordanstavrakis-art <jordan.stavrakis@gmail.com>
2026-07-18 20:52:47 +0100

    Prepping LCD Number Kata files and folder. Also created tempalate test file for future use.

---
+++ LCD Number Display/LCDNumberTest.py
@@ -0,0 +1,10 @@
+import unittest
+from Wordle import target_word, guess, result
+
+class Testing(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
