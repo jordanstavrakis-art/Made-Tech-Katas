805b4d64ce8374d27b7576bf2b616089b7034c7b
jordanstavrakis-art <jordan.stavrakis@gmail.com>
2026-07-19 14:48:51 +0100

    finished first part. Second part not requires width and height options

--- LCD Number Display/LCDNumber.py
+++ LCD Number Display/LCDNumber.py
@@ -1,6 +1,80 @@
 """
 write a program that displays LCD style numbers.
 """
+class zero:
+    top = " _ "
+    middle = "| |"
+    bottom = "|_|"
 
-lcd_number_display = ".\n.\n.\n.\n.\n"
-print(lcd_number_display)
+class one:
+    top = "   "
+    middle = " | "
+    bottom = " | "
+
+class two:
+    top = " _ "
+    middle = " _|"
+    bottom = "|_ "
+
+class three:
+    top = " _ "
+    middle = " _|"
+    bottom = " _|"
+
+class four:
+    top = "   "
+    middle = "|_|"
+    bottom = "  |"
+
+class five:
+    top = " _ "
+    middle = "|_ "
+    bottom = " _|"
+
+class six:
+    top = " _ "
+    middle = "|_ "
+    bottom = "|_|"
+
+class seven:
+    top = " _ "
+    middle = "  |"
+    bottom = "  |"
+
+class eight:
+    top = " _ "
+    middle = "|_|"
+    bottom = "|_|"
+
+class nine:
+    top = " _ "
+    middle = "|_|"
+    bottom = " _|"
+
+numbers_dict = {'0': zero,
+                '1': one,
+                '2': two,
+                '3': three,
+                '4': four,
+                '5': five,
+                '6': six,
+                '7': seven,
+                '8': eight,
+                '9': nine}
+
+def print_LCD(list):
+    sections = ["top","middle","bottom"]
+    lcd_output = ""
+    for s in sections:
+        if s != "top":
+            lcd_output += "\n"
+        for n in list:
+            if n in numbers_dict.keys():
+                lcd_output += getattr(numbers_dict[n],s)
+            else:
+                pass
+    print(lcd_output)
+
+
+number_input = list(input("Enter some numbers: "))
+print_LCD(number_input)
--- LCD Number Display/LCDNumberTest.py
+++ LCD Number Display/LCDNumberTest.py
@@ -1,15 +1,38 @@
 import unittest
-from LCDNumber import lcd_number_display
+import re
+from LCDNumber import zero
 
 class Testing(unittest.TestCase):
+    valid_characters = r"[|_ ]+"
+    zero_top = getattr(zero,"top")
+    zero_middle = getattr(zero,"middle")
+    zero_bottom = getattr(zero,"bottom")
 
-    def setUp(self):
-        pass
+    # def test_lcd_is_3_lines_high(self):
+    #     self.assertIn(getattr(zero,"top"),zero)
+    #     self.assertIn(getattr(zero,"top"),zero)
+    #     self.assertIn(getattr(zero,"top"),zero)
 
-    def test_lcd_string_value_exists(self):
-        self.assertTrue(type(lcd_number_display) == str)
-    def test_lcd_contains_4_line_breaks(self):
-        self.assertTrue(lcd_number_display.count("\n") == 4)
+    def test_lcd_only_contains_correct_characters(self):
+            self.assertTrue(re.fullmatch(self.valid_characters,str(self.zero_top)))
+            self.assertTrue(re.fullmatch(self.valid_characters,str(self.zero_middle)))
+            self.assertTrue(re.fullmatch(self.valid_characters,str(self.zero_bottom)))
+
+    # def test_zero_output_correct(self):
+    #     self.assertTrue(zero.top == " _ ")
+    #     self.assertTrue(zero.middle == "| |")
+    #     self.assertTrue(zero.bottom == "|_|")
+    
+    # def test_zero_width_4(self):
+    #      expected_top = " __ "
+    #      expected_middle = "|  |"
+    #      expected_bottom = "|__|"
+    #      self.assertTrue(self.zero_top == expected_top)
+    #      self.assertTrue(self.zero_middle == expected_middle)
+    #      self.assertTrue(self.zero_bottom == expected_bottom)
+
+         
+
 
 if __name__ == '__main__':
     unittest.main()
