ca31c878c0dabaec68b8e5980c6f4eeab90a1e2d
jordanstavrakis-art <jordan.stavrakis@gmail.com>
2026-07-19 11:41:41 +0100

    Creating branch for LCD Display Kata. Contains initial tests and basic set-up

---
+++ LCD Number Display/LCDNumber.py
@@ -0,0 +1,6 @@
+"""
+write a program that displays LCD style numbers.
+"""
+
+lcd_number_display = ".\n.\n.\n.\n.\n"
+print(lcd_number_display)
--- LCD Number Display/LCDNumberTest.py
+++ LCD Number Display/LCDNumberTest.py
@@ -1,9 +1,15 @@
 import unittest
+from LCDNumber import lcd_number_display
 
 class Testing(unittest.TestCase):
 
     def setUp(self):
         pass
+
+    def test_lcd_string_value_exists(self):
+        self.assertTrue(type(lcd_number_display) == str)
+    def test_lcd_contains_4_line_breaks(self):
+        self.assertTrue(lcd_number_display.count("\n") == 4)
 
 if __name__ == '__main__':
     unittest.main()
